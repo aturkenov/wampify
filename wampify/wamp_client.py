78d36b0fe4a9c0bb056dcbd53b1b87b552f1b8fa
Aidar Turkenov <a.turkenov@orion-m2m.com>
2022-02-21 17:44:04 +0600

    something refactored

--- wampify/wamp_client.py
+++ wampify/wamp_client.py
@@ -6,7 +6,7 @@ import base64
 from random import randint
 from datetime import datetime
 from wampify.exceptions import SomethingWentWrong
-from typing import Mapping
+from typing import Mapping, Tuple
 
 
 def _utcnow():
@@ -28,16 +28,35 @@ class WAMPClient:
     ) -> None:
         assert type(caller_url) == str
         assert type(publisher_url) == str
-        assert type(key) == str
-        assert type(secret) == str
         assert type(timeout) == int
         self._caller = caller_url
         self._publisher = publisher_url
-        self._key = key
-        self._secret = secret
+        self._key = None
+        if key:
+            assert type(key) == str
+            self._key = key
+        self._secret = None
+        if secret:
+            assert type(secret) == str
+            self._secret = secret
         self._timeout = timeout
         self._sequence = 1
         self._headers = { 'Content-Type': 'application/json' }
+
+    def _generate_signature(
+        self,
+        data: bytes
+    ) -> Tuple[str, int, str]:
+        timestamp = _utcnow()
+        nonce = randint(0, 9007199254740992)
+        hm = hmac.new(self._secret.encode('utf8'), None, hashlib.sha256)
+        hm.update(self._key.encode('utf8'))
+        hm.update(timestamp.encode('utf8'))
+        hm.update(str(self._sequence).encode('utf8'))
+        hm.update(str(nonce).encode('utf8'))
+        hm.update(data)
+        signature = base64.urlsafe_b64encode(hm.digest())
+        return timestamp, nonce, signature.decode('utf8')
 
     async def call(
         self,
@@ -58,26 +77,15 @@ class WAMPClient:
             body['options'] = options
         data = json.dumps(body)
 
-        params = {
-            'timestamp': _utcnow(),
-            'seq': self._sequence
-        }
+        P = { 'seq': self._sequence }
         if self._key:
-            params['key'] = self._key
-            params['nonce'] = randint(0, 9007199254740992)
-            hm = hmac.new(self._secret.encode('utf8'), None, hashlib.sha256)
-            hm.update(params['key'].encode('utf8'))
-            hm.update(params['timestamp'].encode('utf8'))
-            hm.update(str(params['seq']).encode('utf8'))
-            hm.update(str(params['nonce']).encode('utf8'))
-            hm.update(data)
-            signature = base64.urlsafe_b64encode(hm.digest())
-            params['signature'] = signature.decode('utf8')
+            P['key'] = self._key
+            P['timestamp'], P['nonce'], P['signature'] = self._generate_signature(data)
 
         self._sequence += 1
 
         async with HTTPClient(headers=self._headers) as session:
-            async with session.post(self._caller, params=params, data=data) as response:
+            async with session.post(self._caller, params=P, data=data) as response:
                 data = await response.read()
                 body = json.loads(data)
                 if response.status == 200:
@@ -103,26 +111,15 @@ class WAMPClient:
             body['options'] = options
         data = json.dumps(body)
 
-        params = {
-            'timestamp': _utcnow(),
-            'seq': self._sequence
-        }
+        P = { 'seq': self._sequence }
         if self._key:
-            params['key'] = self._key
-            params['nonce'] = randint(0, 9007199254740992)
-            hm = hmac.new(self._secret.encode('utf8'), None, hashlib.sha256)
-            hm.update(params['key'].encode('utf8'))
-            hm.update(params['timestamp'].encode('utf8'))
-            hm.update(str(params['seq']).encode('utf8'))
-            hm.update(str(params['nonce']).encode('utf8'))
-            hm.update(data)
-            signature = base64.urlsafe_b64encode(hm.digest())
-            params['signature'] = signature.decode('utf8')
+            P['key'] = self._key
+            P['timestamp'], P['nonce'], P['signature'] = self._generate_signature(data)
 
         self._sequence += 1
 
         async with HTTPClient(headers=self._headers) as session:
-            async with session.post(self._publisher, params=params, data=data) as response:
+            async with session.post(self._publisher, params=P, data=data) as response:
                 data = await response.read()
                 body = json.loads(data)
                 if response.status == 200:
